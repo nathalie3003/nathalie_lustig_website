3fbe942b613b88ace7ec1c7379336ef40197b1f7
Nathalie Lustig <nathalie.lustig03@gmail.com>
2026-07-14 11:38:36 +0100

    Wire article choreography into trade template

--- src/app/notes/[slug]/page.tsx
+++ src/app/notes/[slug]/page.tsx
@@ -43,6 +43,7 @@ export default async function NotePage({
         next={next}
         dateLabel={formatDateLong(note.publishedAt)}
         readLabel={`${minutes} read`}
+        resetKey={slug}
       />
     );
   }
